8d0685ca0cc17168718c3688a616502691ea9fa5
Adrian Johnson <aj@dustify.com>
2024-05-18 14:37:22 +0000

    mid-point

--- packages/mt-stats-srv/src/service/PostgresStorageService.ts
+++ packages/mt-stats-srv/src/service/PostgresStorageService.ts
@@ -1,5 +1,29 @@
 import { IStorageService } from "../if/IStorageService";
+import { ICompleteMessage } from "../model/ICompleteMessage";
+import { IRawMessage } from "../model/IRawMessage";
+import { IUnpackedMessage } from "../model/IUnpackedMessage";
+
+import {} from "pg";
 
 export class PostgresStorageService implements IStorageService {
+    StoreRawMessage(message: IRawMessage): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+    GetRawMessages(): Promise<IRawMessage[]> {
+        throw new Error("Method not implemented.");
+    }
+
+    StoreUnpackedMessage(message: IUnpackedMessage): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+    GetUnpackedMessages(): Promise<IUnpackedMessage[]> {
+        throw new Error("Method not implemented.");
+    }
+
+    StoreCompleteMessage(message: ICompleteMessage): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
 
 }
